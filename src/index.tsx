d29b5952ccf24279b2fba6246dac725cd314de67
luotao <luotaoyeah@gmail.com>
2018-05-20 18:54:07 +0800

    quick start: hello world

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import App from "./App";
+// import * as React from "react";
+// import * as ReactDOM from "react-dom";
+// import App from "./App";
 import "./index.css";
 import "antd/dist/antd.css";
-import './assets/iconfont/iconfont.css';
+import "./assets/iconfont/iconfont.css";
 import registerServiceWorker from "./registerServiceWorker";
+import "./doc/01-quick-start/01-hello-world";
 
-ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
+// ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
 registerServiceWorker();
