199543fe07d3a7485d4895e77e1f6ae341511a14
luotao <luotaoyeah@gmail.com>
2018-08-27 21:49:00 +0800

    doc: state and lifecycle

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { C010405 } from "../doc/01/01-04/01-04-05";
+import { C010501 } from "../doc/01/01-05/01-05-01";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <C010405 />
+        <C010501 />
       </div>
     );
   }
