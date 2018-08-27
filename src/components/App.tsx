637d432f911089c4b6b025f27ed213658187ac29
luotao <luotaoyeah@gmail.com>
2018-08-27 22:55:41 +0800

    doc: state and lifecycle: add lifecycle methods to a class

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { C010503 } from "../doc/01/01-05/01-05-03";
+import { C010504 } from "../doc/01/01-05/01-05-04";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <C010503 />
+        <C010504 />
       </div>
     );
   }
