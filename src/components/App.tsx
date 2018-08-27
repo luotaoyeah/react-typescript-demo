5b276051b6f4c65187b4d790cd42d22b962d16fe
luotao <luotaoyeah@gmail.com>
2018-08-27 21:14:56 +0800

    doc: components and props: extracting components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { C010403 } from "../doc/01/01-04/01-04-03";
+import { C010404 } from "../doc/01/01-04/01-04-04";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <C010403 />
+        <C010404 />
       </div>
     );
   }
