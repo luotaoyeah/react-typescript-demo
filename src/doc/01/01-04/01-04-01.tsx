cd32d711ab589b44ced0b8f840c25b24a2e66302
luotao <luotaoyeah@gmail.com>
2018-08-27 20:08:24 +0800

    update dependencies

---
+++ src/doc/01/01-04/01-04-01.tsx
@@ -0,0 +1,18 @@
+/*
+ * Components and Props
+ */
+
+import * as React from "react";
+
+/*
+ *
+ */
+console.log("\n-------------------------------------------------- 01");
+
+class C010401 extends React.Component {
+  render(): React.ReactNode {
+    return <div>TODO</div>;
+  }
+}
+
+export { C010401 };
