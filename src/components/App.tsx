fb3d16773b5a21720a37a4b102dccabf70217072
luotao <luotaoyeah@gmail.com>
2018-08-27 21:02:18 +0800

    doc: components and props: composing components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { C010402 } from "../doc/01/01-04/01-04-02";
+import { C010403 } from "../doc/01/01-04/01-04-03";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <C010402 />
+        <C010403 />
       </div>
     );
   }
