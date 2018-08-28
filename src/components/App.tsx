3e9783e05295053c29a7fc0392c0728d53c64439
luotao <luotaoyeah@gmail.com>
2018-08-28 21:13:30 +0800

    doc: conditional rendering

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { C010602 } from "../doc/01/01-06/01-06-02";
+import { C010701 } from "../doc/01/01-07/01-07-01";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <C010602 />
+        <C010701 />
       </div>
     );
   }
