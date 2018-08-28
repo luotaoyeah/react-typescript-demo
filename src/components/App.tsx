9ef189242ddc7d077c7822cb9f41f2e37bddb48c
luotao <luotaoyeah@gmail.com>
2018-08-28 20:50:23 +0800

    doc: handling events: passing arguments to event handlers

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { C010601 } from "../doc/01/01-06/01-06-01";
+import { C010602 } from "../doc/01/01-06/01-06-02";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <C010601 />
+        <C010602 />
       </div>
     );
   }
