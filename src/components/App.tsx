4550e8354d76f9e486d2bed0a658b25c7b9a169d
luotao <luotaoyeah@gmail.com>
2018-08-29 20:30:18 +0800

    doc: conditional rendering: element variables

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { C010701 } from "../doc/01/01-07/01-07-01";
+import { C010702 } from "../doc/01/01-07/01-07-02";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <C010701 />
+        <C010702 />
       </div>
     );
   }
