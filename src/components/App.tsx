816c70ec805cdca54115cfddb2fac10a3295cc1c
luotao <luotaoyeah@gmail.com>
2018-08-30 21:32:48 +0800

    doc: lists and keys: keys must only be unique among siblings

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { C010804 } from "../doc/01/01-08/01-08-04";
+import { C010805 } from "../doc/01/01-08/01-08-05";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <C010804 />
+        <C010805 />
       </div>
     );
   }
