59681ce491bb0006cf2648f2c1be75d48261af2b
luotao <luotaoyeah@gmail.com>
2018-08-30 21:07:17 +0800

    doc: lists and keys: extracting components with keys

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { C010803 } from "../doc/01/01-08/01-08-03";
+import { C010804 } from "../doc/01/01-08/01-08-04";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <C010803 />
+        <C010804 />
       </div>
     );
   }
