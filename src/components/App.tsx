ed8cf6fcb0e6cdce0025eadcdb704524602e4096
luotao <luotaoyeah@gmail.com>
2018-08-30 20:14:19 +0800

    doc: lists and keys: rendering multiple components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { C010705 } from "../doc/01/01-07/01-07-05";
+import { C010801 } from "../doc/01/01-08/01-08-01";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <C010705 />
+        <C010801 />
       </div>
     );
   }
