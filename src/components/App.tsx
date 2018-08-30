503f1a81da12e2ceee09870c6c5dd2e1db24a092
luotao <luotaoyeah@gmail.com>
2018-08-30 20:48:52 +0800

    doc: lists and keys: keys

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { C010802 } from "../doc/01/01-08/01-08-02";
+import { C010803 } from "../doc/01/01-08/01-08-03";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <C010802 />
+        <C010803 />
       </div>
     );
   }
