83f0d4ab83564324b4dfd195b9aa9dfa857a8103
luotao <luotaoyeah@gmail.com>
2018-08-31 20:48:10 +0800

    doc: forms: controlled component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { C010901 } from "../doc/01/01-09/01-09-01";
+import { C010902 } from "../doc/01/01-09/01-09-02";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <C010901 />
+        <C010902 />
       </div>
     );
   }
