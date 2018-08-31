4f86b760810b87f9010793897d5c9d8ec85a85bf
luotao <luotaoyeah@gmail.com>
2018-08-31 20:09:11 +0800

    doc: lists and keys: embedding map() in jsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { C010805 } from "../doc/01/01-08/01-08-05";
+import { C010806 } from "../doc/01/01-08/01-08-06";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <C010805 />
+        <C010806 />
       </div>
     );
   }
