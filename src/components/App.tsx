b3c4f43d8be82d17b0d204ca9089e02011522235
luotao <luotaoyeah@gmail.com>
2018-09-01 22:03:44 +0800

    doc: composition vs inheritance: specialization

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { C011101 } from "../doc/01/01-11/01-11-01";
+import { C011102 } from "../doc/01/01-11/01-11-02";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <C011101 />
+        <C011102 />
       </div>
     );
   }
