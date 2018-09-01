9514a2583f6350588a50138d67c720b4c7e59674
luotao <luotaoyeah@gmail.com>
2018-09-01 20:11:37 +0800

    doc: forms: <select>

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { C010903 } from "../doc/01/01-09/01-09-03";
+import { C010904 } from "../doc/01/01-09/01-09-04";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <C010903 />
+        <C010904 />
       </div>
     );
   }
