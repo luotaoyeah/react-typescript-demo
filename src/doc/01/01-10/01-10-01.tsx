4359a7d4a99f80a1fb99826cfcf5d3a53ed7017d
luotao <luotaoyeah@gmail.com>
2018-09-01 21:27:48 +0800

    doc: lifting state up: lifting state up

--- src/doc/01/01-10/01-10-01.tsx
+++ src/doc/01/01-10/01-10-01.tsx
@@ -3,19 +3,19 @@
  */
 
 import * as React from "react";
-import { Tag } from "antd";
 import { SyntheticEvent } from "react";
+import { Tag } from "antd";
 
 /*
  * 如果多个组件需要共享状态数据，可以将状态数据放到离他们最近的相同父组件中；
  */
 
 function toC(f: number): number {
-  return ((f - 32) * 5) / 9;
+  return Math.round((((f - 32) * 5) / 9) * 1000) / 1000;
 }
 
 function toF(c: number): number {
-  return (c * 9) / 5 + 32;
+  return Math.round(((c * 9) / 5 + 32) * 1000) / 1000;
 }
 
 class BoilingVerdict extends React.Component<{ celsius: number }> {
@@ -28,20 +28,18 @@ class BoilingVerdict extends React.Component<{ celsius: number }> {
   }
 }
 
-class TemperatureInput extends React.Component<
-  { scale: "C" | "F" },
-  { temperature: number }
-> {
+class TemperatureInput extends React.Component<{
+  scale: "C" | "F";
+  value: number;
+  onChange: (temperature: number) => void;
+}> {
   constructor(props: any) {
     super(props);
-    this.state = { temperature: 0 };
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(e: SyntheticEvent) {
-    this.setState({
-      temperature: Number((e.target as HTMLInputElement).value)
-    });
+    this.props.onChange(Number((e.target as HTMLInputElement).value));
   }
 
   render(): React.ReactNode {
@@ -50,12 +48,54 @@ class TemperatureInput extends React.Component<
         <legend>temperature in {this.props.scale}</legend>
         <input
           type="number"
-          value={this.state.temperature}
+          value={this.props.value}
           onChange={this.handleChange}
         />
-
-        <BoilingVerdict celsius={this.state.temperature} />
       </fieldset>
+    );
+  }
+}
+
+class Calculator extends React.Component<
+  {},
+  {
+    scale: "C" | "F";
+    temperature: number;
+  }
+> {
+  constructor(props: {}, context: any) {
+    super(props, context);
+    this.state = {
+      scale: "C",
+      temperature: 0
+    };
+    this.handleCChange = this.handleCChange.bind(this);
+    this.handleFChange = this.handleFChange.bind(this);
+  }
+
+  handleCChange(temperature: number) {
+    this.setState({ scale: "C", temperature });
+  }
+
+  handleFChange(temperature: number) {
+    this.setState({ scale: "F", temperature });
+  }
+
+  render(): React.ReactNode {
+    const c =
+      this.state.scale === "F"
+        ? toC(this.state.temperature)
+        : this.state.temperature;
+    const f =
+      this.state.scale === "C"
+        ? toF(this.state.temperature)
+        : this.state.temperature;
+    return (
+      <div>
+        <TemperatureInput value={c} scale={"C"} onChange={this.handleCChange} />
+        <TemperatureInput value={f} scale={"F"} onChange={this.handleFChange} />
+        <BoilingVerdict celsius={c} />
+      </div>
     );
   }
 }
@@ -68,8 +108,7 @@ class C011001 extends React.Component<IProps, IState> {
   render(): React.ReactNode {
     return (
       <div>
-        <TemperatureInput scale={"C"} />
-        <TemperatureInput scale={"F"} />
+        <Calculator />
       </div>
     );
   }
