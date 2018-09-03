a7bc8d7566d113f699dbfd4681685f5b27da480b
luotao <luotaoyeah@gmail.com>
2018-09-03 20:08:25 +0800

    doc: accessibility

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { C011102 } from "../doc/01/01-11/01-11-02";
+import { C010201 } from "../doc/02/02-01/02-01";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <C011102 />
+        <C010201 />
       </div>
     );
   }
