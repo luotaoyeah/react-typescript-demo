956c7bf19cbf71e1f0a3e064972de824aec6ebb7
luotao <luotaoyeah@gmail.com>
2018-09-04 20:57:37 +0800

    doc: context: consuming multiple contexts

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { C020305 } from "../doc/02/02-03/02-03-05";
+import { C020306 } from "../doc/02/02-03/02-03-06";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <C020305 />
+        <C020306 />
       </div>
     );
   }
