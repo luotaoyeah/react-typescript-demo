2f291ea621c9cd4e8696aa564770e93c7aa4d74d
luotao <luotaoyeah@gmail.com>
2018-09-04 20:02:25 +0800

    doc: context: before you use context

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { C020301 } from "../doc/02/02-03/02-03-01";
+import { C020302 } from "../doc/02/02-03/02-03-02";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <C020301 />
+        <C020302 />
       </div>
     );
   }
