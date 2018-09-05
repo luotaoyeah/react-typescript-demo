019163c5c9378bd96b8e49567a8785c0468693ef
luotao <luotaoyeah@gmail.com>
2018-09-05 21:40:48 +0800

    doc: context: consuming context with a HOC

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { C020307 } from "../doc/02/02-03/02-03-07/02-03-07";
+import { C020308 } from "../doc/02/02-03/02-03-08";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <C020307 />
+        <C020308 />
       </div>
     );
   }
