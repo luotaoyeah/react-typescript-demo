b45f859cde9cafa0d5f9bf28899a2222c5c055c1
luotao <luotaoyeah@gmail.com>
2018-09-06 20:24:02 +0800

    upgrade react@16.5.0

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { C020403 } from "../doc/02/02-04/02-04-03";
+import { C020404 } from "../doc/02/02-04/02-04-04";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <C020403 />
+        <C020404 />
       </div>
     );
   }
---
+++ src/doc/02/02-04/02-04-04.tsx
@@ -0,0 +1,13 @@
+/*
+ * Error Boundaries: How About Event Handlers?
+ */
+
+import * as React from "react";
+
+class C020404 extends React.Component {
+  render(): React.ReactNode {
+    return <div />;
+  }
+}
+
+export { C020404 };
