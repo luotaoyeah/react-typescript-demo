5082b218fbb1a08f2705973b51c66e2c0f9966ae
luotao <luotaoyeah@gmail.com>
2018-09-07 20:20:08 +0800

    doc: forwarding refs: forward ref to HOC

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { C020501 } from "../doc/02/02-05/02-05-01";
+import { C020502 } from "../doc/02/02-05/02-05-02";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <C020501 />
+        <C020502 />
       </div>
     );
   }
---
+++ src/doc/02/02-05/02-05-02.tsx
@@ -0,0 +1,80 @@
+/*
+ * Forwarding Refs: Forwarding refs in higher-order components
+ */
+
+import * as React from "react";
+
+/**
+ *
+ */
+const FancyButton = React.forwardRef(
+  (props: { onClick: () => void }, ref?: React.Ref<HTMLButtonElement>) => {
+    return (
+      <button onClick={props.onClick} ref={ref}>
+        button
+      </button>
+    );
+  }
+);
+
+/**
+ * HOC
+ * @param Component
+ * @constructor
+ */
+function LoggerHOC(Component: React.ComponentType<any>) {
+  /*
+   * 通过一个普通的 props 属性 forwardRef 将 ref 传递进来，
+   * 然后继续传递给下级组件；
+   */
+  class LoggerComponent extends React.Component<{
+    forwardRef: React.RefObject<HTMLButtonElement>;
+  }> {
+    componentDidMount() {
+      console.log("LOGGER");
+    }
+
+    render(): React.ReactNode {
+      return <Component {...this.props} ref={this.props.forwardRef} />;
+    }
+  }
+
+  return React.forwardRef((props: any, ref?: React.Ref<HTMLButtonElement>) => {
+    return <LoggerComponent {...props} forwardRef={ref} />;
+  });
+}
+
+/**
+ *
+ */
+const LoggerFancyButton = LoggerHOC(FancyButton);
+
+/**
+ *
+ */
+class C020502 extends React.Component {
+  constructor(props: {}, context: any) {
+    super(props, context);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  ref: React.RefObject<HTMLButtonElement> = React.createRef();
+
+  handleClick() {
+    const vm = this;
+    if (vm.ref.current) {
+      vm.ref.current.style.color = "red";
+      vm.ref.current.style.borderRadius = "3px";
+    }
+  }
+
+  render(): React.ReactNode {
+    return (
+      <div>
+        <LoggerFancyButton ref={this.ref} onClick={this.handleClick} />
+      </div>
+    );
+  }
+}
+
+export { C020502 };
