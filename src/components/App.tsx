5f16e9372fb3b8c7895b6cd8008aaf864b2b6952
luotao <luotaoyeah@gmail.com>
2018-09-08 09:39:48 +0800

    doc: HOC

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { C020603 } from "../doc/02/02-06/02-06-03";
+import { C020701 } from "../doc/02/02-07/02-07-01";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <C020603 />
+        <C020701 />
       </div>
     );
   }
