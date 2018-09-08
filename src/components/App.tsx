e08f11d36863a7da71f72090b3bc95f67208de09
luotao <luotaoyeah@gmail.com>
2018-09-08 10:28:45 +0800

    doc: HOC: caveats

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { C020704 } from "../doc/02/02-07/02-07-04";
+import { C020705 } from "../doc/02/02-07/02-07-05";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <C020704 />
+        <C020705 />
       </div>
     );
   }
