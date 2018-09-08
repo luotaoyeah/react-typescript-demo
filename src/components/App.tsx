6009bb789b7e6ef400e413453645be861b2f31e7
luotao <luotaoyeah@gmail.com>
2018-09-08 09:51:12 +0800

    doc: HOC: don't mutate original component, use composition

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { C020701 } from "../doc/02/02-07/02-07-01";
+import { C020702 } from "../doc/02/02-07/02-07-02";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <C020701 />
+        <C020702 />
       </div>
     );
   }
---
+++ src/doc/02/02-07/02-07-02.tsx
@@ -0,0 +1,37 @@
+/*
+ * Higher-Order Components: Don’t Mutate the Original Component. Use Composition
+ */
+
+import * as React from "react";
+import { Button } from "antd";
+
+/*
+ * 在 HOC 中，不要对参数组件做任何修改，
+ * 而是应该通过组合（composition）的方式来增强组件；
+ */
+function HOC<P>(Component: React.ComponentType<P>) {
+  /*
+   * 修改了参数组件的原型对象；
+   */
+  Component.prototype.componentDidMount = () => {
+    console.log("LOG");
+  };
+
+  return Component;
+}
+
+/**  */
+const HocButton = HOC<any>(Button);
+
+/**  */
+class C020702 extends React.Component {
+  render(): React.ReactNode {
+    return (
+      <div>
+        <HocButton>button</HocButton>
+      </div>
+    );
+  }
+}
+
+export { C020702 };
