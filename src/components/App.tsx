4a982cd2250edc27870f02193059fc06377d0223
luotao <luotaoyeah@gmail.com>
2018-09-09 09:14:37 +0800

    react-router: basic components: <Switch> <Route>

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { C0102 } from "../react-router/guides/01/01-02";
+import { C0202 } from "../react-router/guides/02/02-02";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <C0102 />
+        <C0202 />
       </div>
     );
   }
