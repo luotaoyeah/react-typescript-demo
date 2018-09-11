aa4391ac36d8acb60d4178ea62ced3d7e7a2999a
luotao <luotaoyeah@gmail.com>
2018-09-11 20:33:53 +0800

    doc: JSX: React.createElement()

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { C020802 } from "../doc/02/02-08/02-08-02/02-08-02";
+import { C020901 } from "../doc/02/02-09/02-09-01";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <C020802 />
+        <C020901 />
       </div>
     );
   }
