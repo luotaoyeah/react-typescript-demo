fdb9ebb79bbe48467c46e9395cbc940305bd09a1
luotao <luotaoyeah@gmail.com>
2018-09-12 20:32:17 +0800

    doc: JSX: children: true, false, null, undefined are ignored

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { C020906 } from "../doc/02/02-09/02-09-06";
+import { C020907 } from "../doc/02/02-09/02-09-07";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <C020906 />
+        <C020907 />
       </div>
     );
   }
