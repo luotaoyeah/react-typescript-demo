361d2d99c9ad97a0aac6e3dd124758b518e0f474
luotao <luotaoyeah@gmail.com>
2018-09-12 20:17:48 +0800

    doc: JSX: children: function as children

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { C020905 } from "../doc/02/02-09/02-09-05";
+import { C020906 } from "../doc/02/02-09/02-09-06";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <C020905 />
+        <C020906 />
       </div>
     );
   }
