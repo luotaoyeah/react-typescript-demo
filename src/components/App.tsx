d99616069b4cc6b1024d20effdc461111c5f6d4f
luotao <luotaoyeah@gmail.com>
2018-09-13 20:14:00 +0800

    doc: refs and the dom: adding a ref to a DOM element

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { C021501 } from "../doc/02/02-15/02-15-01";
+import { C021502 } from "../doc/02/02-15/02-15-02";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <C021501 />
+        <C021502 />
       </div>
     );
   }
