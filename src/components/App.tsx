9df06b16eeebb1d1ff3d14464ac5ffa704f06f60
luotao <luotaoyeah@gmail.com>
2018-09-13 20:40:29 +0800

    doc: refs and the dom: callback refs

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { C021504 } from "../doc/02/02-15/02-15-04";
+import { C021505 } from "../doc/02/02-15/02-15-05";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <C021504 />
+        <C021505 />
       </div>
     );
   }
