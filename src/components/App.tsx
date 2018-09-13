a634f914a1dde2d496c40e0c4bc7343199cb0d0b
luotao <luotaoyeah@gmail.com>
2018-09-13 20:06:18 +0800

    doc: refs and the dom: React.createRef()

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { C021102 } from "../doc/02/02-11/02-11-02";
+import { C021501 } from "../doc/02/02-15/02-15-01";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <C021102 />
+        <C021501 />
       </div>
     );
   }
