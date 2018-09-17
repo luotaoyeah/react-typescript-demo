d3c6f1cfc78868db56d28dfddf472869aff281e4
luotao <luotaoyeah@gmail.com>
2018-09-17 20:00:52 +0800

    doc: render props

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { C021505 } from "../doc/02/02-15/02-15-05";
+import { C021601 } from "../doc/02/02-16/02-16-01/02-16-01";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <C021505 />
+        <C021601 />
       </div>
     );
   }
---
+++ src/doc/02/02-16/02-16-01/02-16-01.tsx
@@ -0,0 +1,52 @@
+/*
+ * Render Props
+ */
+
+import React from "react";
+import { Button } from "antd";
+
+/*
+ * render props 指的是一种特殊的 props，
+ * 这个属性通常取名为 render，它的作用是：
+ *     覆盖组件自身的 render() 方法；
+ *
+ * render props 接收组件的 state 对象作为参数，
+ * 返回一个 React 元素，组件可以使用该 render props 个性化组件的渲染，
+ * 即将组件的渲染逻辑参数化，使用外界传入的方法来进行渲染；
+ */
+
+interface IAProps {
+  render?: (state: IAState) => React.ReactNode;
+}
+
+interface IAState {
+  text: string;
+}
+
+/**  */
+class A extends React.Component<IAProps, IAState> {
+  state: IAState = {
+    text: "A"
+  };
+
+  render(): React.ReactNode {
+    if (this.props.render) {
+      return this.props.render(this.state);
+    }
+
+    return <i>{this.state.text}</i>;
+  }
+}
+
+/**  */
+class C021601 extends React.Component {
+  renderA(state: IAState) {
+    return <Button>{state.text}</Button>;
+  }
+
+  render(): React.ReactNode {
+    return <A render={this.renderA} />;
+  }
+}
+
+export { C021601 };
