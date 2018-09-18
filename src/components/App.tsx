fa752651fb0a0af5c9798cdcbae48a91a4490a81
luotao <luotaoyeah@gmail.com>
2018-09-18 20:24:11 +0800

    react-router: api: <Route>: Route

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { C021603A } from "../doc/02/02-16/02-16-03/02-16-03-A";
+import { ApiRouteComponent } from "../react-router/api/Route/component";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <C021603A />
+        <ApiRouteComponent />
       </div>
     );
   }
