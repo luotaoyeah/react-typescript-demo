56bbc83e8353ab651a3bd43f6d3e6c7056938313
luotao <luotaoyeah@gmail.com>
2018-09-19 20:02:05 +0800

    doc: strict mode: React.StrictMode

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { ApiRouteChildren } from "../react-router/api/Route/children";
+import { C021801 } from "../doc/02/02-18/02-18-01/02-18-01";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <ApiRouteChildren />
+        <C021801 />
       </div>
     );
   }
