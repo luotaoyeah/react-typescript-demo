e6b721cb1b072a2fb3ba1401c679cd8ddc87c7ab
luotao <luotaoyeah@gmail.com>
2018-09-20 20:33:19 +0800

    doc: uncontrolled components: <input type="file"/>

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { C022002 } from "../doc/02/02-20/02-20-02";
+import { C022003 } from "../doc/02/02-20/02-20-03";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <C022002 />
+        <C022003 />
       </div>
     );
   }
