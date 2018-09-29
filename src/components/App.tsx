458c8d909c5f8bc4a3164dda69db6d6d2acbebec
luotao <luotaoyeah@gmail.com>
2018-09-29 20:57:04 +0800

    doc: api: React.ReactChildren.toArray()

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { C030109 } from "../doc/03/03-01/03-01-09";
+import { C030110 } from "../doc/03/03-01/03-01-10";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <C030109 />
+        <C030110 />
       </div>
     );
   }
