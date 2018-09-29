ce7cc27ca71a8b8f43eac5b29d44701e7ed4d7b6
luotao <luotaoyeah@gmail.com>
2018-09-29 20:12:49 +0800

    doc: api: React.cloneElement()

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { C030103 } from "../doc/03/03-01/03-01-03";
+import { C030104 } from "../doc/03/03-01/03-01-04";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <C030103 />
+        <C030104 />
       </div>
     );
   }
