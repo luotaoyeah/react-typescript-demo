f8492085240ee1107237cd12c2f261ae114e9cb5
luotao <luotaoyeah@gmail.com>
2018-10-22 21:50:03 +0800

    doc: api: React.Component: render(): return React.ReactPortal

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { C0302030103 } from "../doc/03/03_02/03_02_03/03_02_03_01/C0302030103";
+import { C0302030104 as C } from "../doc/03/03_02/03_02_03/03_02_03_01/C0302030104";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <C0302030103 />
+        <C />
       </div>
     );
   }
---
+++ src/doc/03/03_02/03_02_03/03_02_03_01/C0302030104.tsx
@@ -0,0 +1,37 @@
+/*
+ * React.Component
+ *     Reference
+ *         render()
+ */
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { Button } from "antd";
+
+class A extends React.Component {
+  /*
+   * render() 方法可以返回一个 React.ReactPortal；
+   */
+  render(): React.ReactPortal {
+    return ReactDOM.createPortal(
+      <Button>portal</Button>,
+      document.getElementById("root02")!
+    );
+  }
+}
+
+interface IProps {}
+
+interface IState {}
+
+class C0302030104 extends React.Component<IProps, IState> {
+  render(): React.ReactNode {
+    return (
+      <div>
+        <A />
+      </div>
+    );
+  }
+}
+
+export { C0302030104 };
