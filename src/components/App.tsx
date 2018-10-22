612233a042fed014047fd95e0307c7deba951307
luotao <luotaoyeah@gmail.com>
2018-10-22 20:38:52 +0800

    doc: api: React.Component: lifecycle: mounting

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { C03010901 } from "../doc/03/03_01/03_01_09/C03010901";
+import { C03020201 } from "../doc/03/03_02/03_02_02/C03020201";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <C03010901 />
+        <C03020201 />
       </div>
     );
   }
