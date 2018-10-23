4935f13704da21f3718be280039b819aa7dd39fc
luotao <luotaoyeah@gmail.com>
2018-10-23 21:21:35 +0800

    doc: api: React.Component: constructor(): avoid copying props into state

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { C0302030202 as C } from "../doc/03/03_02/03_02_03/03_02_03_02/C0302030202";
+import { C0302030203 as C } from "../doc/03/03_02/03_02_03/03_02_03_02/C0302030203";
 
 class App extends React.Component {
   public render() {
---
+++ src/doc/03/03_02/03_02_03/03_02_03_02/C0302030203.tsx
@@ -0,0 +1,42 @@
+/*
+ * React.Component
+ *     Reference
+ *         constructor()
+ */
+
+import React from "react";
+import { Button } from "antd";
+
+interface IProps {
+  color?: string;
+}
+
+interface IState {}
+
+class A extends React.Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+    /*
+     * 在 constructor() 中初始化 state 时，不要直接使用 props 数据；
+     */
+    /*
+        this.state = { color: props.color };
+    */
+  }
+
+  render(): React.ReactNode {
+    return <Button>A</Button>;
+  }
+}
+
+class C0302030203 extends React.Component {
+  render(): React.ReactNode {
+    return (
+      <div>
+        <A />
+      </div>
+    );
+  }
+}
+
+export { C0302030203 };
