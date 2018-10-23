d6aa74992303b8f8d4e883cd09c06f7215a3ff79
luotao <luotaoyeah@gmail.com>
2018-10-23 21:58:54 +0800

    doc: api: React.Component: componentDidMount(): do not setState() in componentDidMount()

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { C0302030301 as C } from "../doc/03/03_02/03_02_03/03_02_03_03/C0302030301";
+import { C0302030302 as C } from "../doc/03/03_02/03_02_03/03_02_03_03/C0302030302";
 
 class App extends React.Component {
   public render() {
---
+++ src/doc/03/03_02/03_02_03/03_02_03_03/C0302030302.tsx
@@ -0,0 +1,52 @@
+/*
+ * React.Component
+ *     Reference
+ *         componentDidMount()
+ */
+
+import React from "react";
+import { Button } from "antd";
+
+interface IProps {}
+
+interface IState {
+  name: string;
+}
+
+class A extends React.Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+    this.state = {
+      name: "A"
+    };
+  }
+
+  componentDidMount(): void {
+    const vm = this;
+    /*
+     * 在 componentDidMount() 中调用 setState() 方法，会立即触发 render() 方法再次调用；
+     * 一般在‘需要获取 DOM 节点信息来初始化相关数据’的情况下，才这样做；
+     * 其他情况下没有必要，且会造成性能问题；
+     */
+    vm.setState({
+      name: "B"
+    });
+  }
+
+  render(): React.ReactNode {
+    console.log(`render(): ${this.state.name}`);
+    return <Button>{this.state.name}</Button>;
+  }
+}
+
+class C0302030302 extends React.Component {
+  render(): React.ReactNode {
+    return (
+      <div>
+        <A />
+      </div>
+    );
+  }
+}
+
+export { C0302030302 };
