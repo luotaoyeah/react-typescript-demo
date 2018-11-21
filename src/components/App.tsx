18b50dacd5d34f3387d80fb9f677ed9938e9df3b
luotao <luotaoyeah@gmail.com>
2018-11-21 19:05:50 +0800

    doc: api: React.Component: componentDidUpdate(): getSnapshotBeforeUpdate()

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { C0302030401 as C } from "../doc/03-api-reference/03-02/03-02-03/03-02-03-04/C0302030401";
+import { C0302030402 as C } from "../doc/03-api-reference/03-02/03-02-03/03-02-03-04/C0302030402";
 
 class App extends React.Component {
   public render() {
