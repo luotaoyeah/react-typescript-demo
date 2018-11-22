8c7f6c69c9efa4b67564c2252f7141b6f3494473
luotao <luotaoyeah@gmail.com>
2018-11-22 21:18:45 +0800

    doc: api: React.Component: forceUpdate()

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { C0302031001 as C } from "../doc/03-api-reference/03-02/03-02-03/03-02-03-10/C0302031001";
+import { C0302031101 as C } from "../doc/03-api-reference/03-02/03-02-03/03-02-03-11/C0302031101";
 
 class App extends React.Component {
   public render() {
