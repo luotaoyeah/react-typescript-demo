c08fca2bd24964e812a46554e402abaec212e807
luotao <luotaoyeah@gmail.com>
2018-11-22 21:00:31 +0800

    doc: api: React.Component: setState()

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { C0302030901 as C } from "../doc/03-api-reference/03-02/03-02-03/03-02-03-09/C0302030901";
+import { C0302031001 as C } from "../doc/03-api-reference/03-02/03-02-03/03-02-03-10/C0302031001";
 
 class App extends React.Component {
   public render() {
