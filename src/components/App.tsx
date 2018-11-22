aaeae0d59d2b7e6ba74c18138b0287b570ff6691
luotao <luotaoyeah@gmail.com>
2018-11-22 20:36:53 +0800

    doc: api: React.Component: componentDidCatch()

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { C0302030801 as C } from "../doc/03-api-reference/03-02/03-02-03/03-02-03-08/C0302030801";
+import { C0302030901 as C } from "../doc/03-api-reference/03-02/03-02-03/03-02-03-09/C0302030901";
 
 class App extends React.Component {
   public render() {
