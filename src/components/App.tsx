6970819e612282d34d58b17f57efad5a5a4650cc
luotao <luotaoyeah@gmail.com>
2018-11-24 11:27:14 +0800

    doc: api: React.Component: displayName

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { C0302040101 as C } from "../doc/03-api-reference/03-02/03-02-04/03-02-04-01/C0302040101";
+import { C0302040201 as C } from "../doc/03-api-reference/03-02/03-02-04/03-02-04-02/C0302040201";
 
 class App extends React.Component {
   public render() {
