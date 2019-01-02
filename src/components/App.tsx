70339791cbf52c29d82852db959f049bcbc0663e
luotao <luotaoyeah@gmail.com>
2019-01-02 21:16:01 +0800

    doc: api: ReactDOM: render()

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { C0302040201 as C } from "../doc/03-api-reference/03-02/03-02-04/03-02-04-02/C0302040201";
+import { C03030101 as C } from "../doc/03-api-reference/03-03-react-dom/03-03-01-render/C03030101";
 
 class App extends React.Component {
   public render() {
--- src/doc/03-api-reference/03-02-react-component/03-02-04/03-02-04-02/C0302040201.tsx
+++ src/doc/03-api-reference/03-02-react-component/03-02-04/03-02-04-02/C0302040201.tsx
@@ -15,7 +15,7 @@ interface IState {}
 class A extends React.Component<IProps, IState> {
   /*
    * 静态属性 displayName 主要用来方便调试，
-   * 如果指定了该属性，则在 devtools 中显示的就是该属性；
+   * 如果指定了该属性，则在 devtools 中显示的就是该属性
    */
   static displayName = "A-A-A";
 
