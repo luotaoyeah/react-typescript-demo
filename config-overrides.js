a927b0de8acc2235b1fb2ce5c83ac92ba6f9acdf
luotao <luotaoyeah@gmail.com>
2019-01-09 21:28:48 +0800

    react-scripts@2.1.3

--- config-overrides.js
+++ config-overrides.js
@@ -1,15 +1,6 @@
-const rewireLess = require("react-app-rewire-less");
 const webpack = require("webpack");
 
-module.exports = function override(config, env) {
-  /*
-   * 覆盖 antd 的样式
-   */
-  config = rewireLess.withLoaderOptions({
-    modifyVars: { "@border-radius-base": "3px" },
-    javascriptEnabled: true
-  })(config, env);
-
+module.exports = function override(config) {
   config.plugins = (config.plugins || []).concat([
     new webpack.ProvidePlugin({
       $: "jquery",
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import "antd/dist/antd.less";
+import "antd/dist/antd.css";
 import "./assets/iconfont/iconfont.css";
 import registerServiceWorker from "./components/registerServiceWorker";
 import App from "./components/App";
