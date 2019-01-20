c7f3a10da498a62b2f069c339ebd3cd95cee8af6
luotao <luotaoyeah@gmail.com>
2019-01-20 11:26:02 +0800

    doc: api: DOM: htmlFor

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { C03040103 as C } from "../doc/03-api-reference/03-04-dom-elements/03-04-01-differences-in-attributes/C03040103";
+import { C03040104 as C } from "../doc/03-api-reference/03-04-dom-elements/03-04-01-differences-in-attributes/C03040104";
 
 class App extends React.Component {
   public render() {
