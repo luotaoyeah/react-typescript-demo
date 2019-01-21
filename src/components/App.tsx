a06053ddd4706dc3c4e601a7eea767538969fa14
luotao <luotaoyeah@gmail.com>
2019-01-21 17:26:01 +0800

    doc: api: DOM: value

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { C03040108 as C } from "../doc/03-api-reference/03-04-dom-elements/03-04-01-differences-in-attributes/C03040108";
+import { C03040109 as C } from "../doc/03-api-reference/03-04-dom-elements/03-04-01-differences-in-attributes/C03040109";
 
 class App extends React.Component {
   public render() {
