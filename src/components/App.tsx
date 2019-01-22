fe3cebea28aa1d31205f9d45b8b0878824641084
luotao <luotaoyeah@gmail.com>
2019-01-22 18:17:06 +0800

    doc: api: SyntheticEvent: nativeEvent

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { C03050101 as COMP } from "../doc/03-api-reference/03-05-synthetic-event/03-05-01-overview/C03050101";
+import { C03050102 as COMP } from "../doc/03-api-reference/03-05-synthetic-event/03-05-01-overview/C03050102";
 
 class App extends React.Component {
   public render() {
