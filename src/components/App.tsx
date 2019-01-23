8cda053f94caccfb68cce642db3736178ebd6ee6
luotao <luotaoyeah@gmail.com>
2019-01-23 17:12:44 +0800

    doc: api: hook: useState()

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { C040101 as COMP } from "../doc/04-hooks/04-01-introducing-hooks/C040101";
+import { C04020101 as COMP } from "../doc/04-hooks/04-02-hooks-at-a-glance/04-02-01-state-hook/C04020101";
 
 class App extends React.Component {
   public render() {
