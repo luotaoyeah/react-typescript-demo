5a1a8ee475df94ac80aaeab19c821a40fc42b9d9
luotao <luotaoyeah@gmail.com>
2019-01-23 15:44:16 +0800

    doc: api: SyntheticEvent: React.BaseSyntheticEvent.persist()

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { C03050201 as COMP } from "../doc/03-api-reference/03-05-synthetic-event/03-05-02-event-pooling/C03050201";
+import { C03050202 as COMP } from "../doc/03-api-reference/03-05-synthetic-event/03-05-02-event-pooling/C03050202";
 
 class App extends React.Component {
   public render() {
