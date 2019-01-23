1d2a4a862850971db5b5c312a557f70a8f5e158e
luotao <luotaoyeah@gmail.com>
2019-01-23 17:27:01 +0800

    doc: api: hook: useState(): multiple declaration

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { C04020101 as COMP } from "../doc/04-hooks/04-02-hooks-at-a-glance/04-02-01-state-hook/C04020101";
+import { C04020102 as COMP } from "../doc/04-hooks/04-02-hooks-at-a-glance/04-02-01-state-hook/C04020102";
 
 class App extends React.Component {
   public render() {
