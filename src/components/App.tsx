6a09f050f80b2f7f9701adef0d0a518d48a83aed
luotao <luotaoyeah@gmail.com>
2019-01-24 21:10:38 +0800

    doc: api: hook: custom hook

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { C04020301 as COMP } from "../doc/04-hooks/04-02-hooks-at-a-glance/04-02-03-rules-of-hooks/C04020301";
+import { C04020401 as COMP } from "../doc/04-hooks/04-02-hooks-at-a-glance/04-02-04-building-your-own-hooks/C04020401";
 
 class App extends React.Component {
   public render() {
