e524bb1dcb978ae90b7f1c038ea9ff3790de6904
luotao <luotaoyeah@gmail.com>
2019-01-24 17:56:18 +0800

    doc: api: hook: rules

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { C04020203 as COMP } from "../doc/04-hooks/04-02-hooks-at-a-glance/04-02-02-effect-hook/C04020203";
+import { C04020301 as COMP } from "../doc/04-hooks/04-02-hooks-at-a-glance/04-02-03-rules-of-hooks/C04020301";
 
 class App extends React.Component {
   public render() {
