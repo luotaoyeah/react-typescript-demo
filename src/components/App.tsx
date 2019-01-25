fcedc1bf7e15cd5d5d7bc4d8bd329235cc13321b
luotao <luotaoyeah@gmail.com>
2019-01-25 17:22:09 +0800

    doc: api: hook: useState(): hooks and function components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { C04030101 as COMP } from "../doc/04-hooks/04-03-using-the-state-hook/04-03-01-equivalent-class-example/C04030101";
+import { C04030201 as COMP } from "../doc/04-hooks/04-03-using-the-state-hook/04-03-02-hooks-and-function-components/C04030201";
 
 class App extends React.Component {
   public render() {
---
+++ src/doc/04-hooks/04-03-using-the-state-hook/04-03-02-hooks-and-function-components/C04030201.tsx
@@ -0,0 +1,39 @@
+/*
+ * Using the State Hook
+ *     Hooks and Function Components
+ */
+
+import React, { useState } from "react";
+import { Button } from "antd";
+
+/*
+ * 在 hooks 出现之前，function 组件又称为 stateless 组件，
+ * 然而有了 hooks 之后，在 function 组件中也可以使用 state 了，
+ * 并且 hooks 只能在 function 组件中使用，不能在 class 组件中使用
+ */
+
+const H01 = (): React.ReactElement<{}> => {
+  const [count, setCount] = useState<number>(0);
+
+  return (
+    <Button
+      onClick={() => {
+        setCount(count + 1);
+      }}
+    >
+      {count}
+    </Button>
+  );
+};
+
+class C04030201 extends React.Component<{}, {}> {
+  render(): React.ReactNode {
+    return (
+      <div>
+        <H01 />
+      </div>
+    );
+  }
+}
+
+export { C04030201 };
