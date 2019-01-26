43dd015d97a3ca5ed8464ee3a3f75305cd5386e8
luotao <luotaoyeah@gmail.com>
2019-01-26 21:45:57 +0800

    doc: api: hook: useEffect(): effects with cleanup: example using classes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { C04040202 as COMP } from "../doc/04-hooks/04-04-using-the-effect-hook/04-04-02-effects-without-cleanup/C04040202";
+import { C04040301 as COMP } from "../doc/04-hooks/04-04-using-the-effect-hook/04-04-03-effects-with-cleanup/C04040301";
 
 class App extends React.Component {
   public render() {
