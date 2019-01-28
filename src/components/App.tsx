73c7d6cac11f9f63c7a6cf892a91c776e3a63a6c
luotao <luotaoyeah@gmail.com>
2019-01-28 16:35:19 +0800

    doc: api: hook: useEffect(): tip: use multiple effects to separate concerns: hooks

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { C04040401 as COMP } from "../doc/04-hooks/04-04-using-the-effect-hook/04-04-04-tips-for-using-effects/C04040401";
+import { C04040402 as COMP } from "../doc/04-hooks/04-04-using-the-effect-hook/04-04-04-tips-for-using-effects/C04040402";
 
 class App extends React.Component {
   public render() {
---
+++ src/doc/04-hooks/04-04-using-the-effect-hook/04-04-04-tips-for-using-effects/C04040402.tsx
@@ -0,0 +1,64 @@
+/*
+ * Using the Effect Hook
+ *     Tips for Using Effects
+ *         Tip: Use Multiple Effects to Separate Concerns
+ */
+
+import React, { useEffect, useState } from "react";
+import { Button, Divider } from "antd";
+
+/*
+ * 使用 hooks 之后，相同的业务逻辑可以统一放到同一个 useEffect() 里面
+ */
+function F01(): React.ReactElement<{}> {
+  const [count, setCount] = useState<number>(0);
+  const [size, setSize] = useState<{ width: number; height: number }>({
+    width: window.innerWidth,
+    height: window.innerHeight
+  });
+
+  useEffect(() => {
+    document.title = String(count);
+  });
+
+  const cb = () => {
+    setSize({
+      width: window.innerWidth,
+      height: window.innerHeight
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", cb);
+
+    return () => {
+      window.removeEventListener("resize", cb);
+    };
+  });
+
+  return (
+    <div>
+      <Button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        {count}
+      </Button>
+      <Divider />
+      <Button>{`${size.width}, ${size.height}`}</Button>
+    </div>
+  );
+}
+
+class C04040402 extends React.Component<{}, {}> {
+  render(): React.ReactNode {
+    return (
+      <div>
+        <F01 />
+      </div>
+    );
+  }
+}
+
+export { C04040402 };
