872e43cda79c132bab1418b8b11f15a4c04e1952
luotao <luotaoyeah@gmail.com>
2019-01-30 16:59:10 +0800

    doc: api: hook: tip: pass information between hooks

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { C04060102 as COMP } from "../doc/04-hooks/04-06-building-your-own-hooks/04-06-01/C04060102";
+import { C04060201 as COMP } from "../doc/04-hooks/04-06-building-your-own-hooks/04-06-02/C04060201";
 
 class App extends React.Component {
   public render() {
