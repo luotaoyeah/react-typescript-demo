be80d9c9dfbe9d9dacfd15c240d75dd3ca7fe7be
luotao <luotaoyeah@gmail.com>
2019-01-30 17:40:04 +0800

    doc: api: hook api: useState(): lazy initialization

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { C04070101 as COMP } from "../doc/04-hooks/04-07-hooks-api-reference/04-07-01/C04070101";
+import { C04070102 as COMP } from "../doc/04-hooks/04-07-hooks-api-reference/04-07-01/C04070102";
 
 class App extends React.Component {
   public render() {
