466cb334740053f4f7909fc4e7437a66496e0ae8
luotao <luotaoyeah@gmail.com>
2019-06-30 21:46:10 +0800

    test: config test coverage for jest

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,7 @@ module.exports = {
   globals: {
     ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: false,
   },
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!**/node_modules/**'],
+  coverageReporters: ['lcovonly', 'text-summary'],
 };
