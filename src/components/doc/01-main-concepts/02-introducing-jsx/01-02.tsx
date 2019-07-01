1a174fca38aea98898f29331721cd4b548340569
luotao <luotaoyeah@gmail.com>
2019-07-01 21:51:45 +0800

    refactor(doc.main-concepts): jsx is an expression too

--- src/components/doc/01-main-concepts/02-introducing-jsx/01-02.tsx
+++ src/components/doc/01-main-concepts/02-introducing-jsx/01-02.tsx
@@ -3,6 +3,7 @@ import { Collapse, Icon } from 'antd';
 import { C010201 } from './01/01-02-01';
 import './01-02.less';
 import { C010202 } from '@/components/doc/01-main-concepts/02-introducing-jsx/02/01-02-02';
+import { C010203 } from '@/components/doc/01-main-concepts/02-introducing-jsx/03/01-02-03';
 
 /**
  * https://reactjs.org/docs/introducing-jsx.html
@@ -16,8 +17,13 @@ export default function C0102(): React.ReactNode {
       <Collapse.Panel header="Why JSX?" key="1">
         {C010201()}
       </Collapse.Panel>
+
       <Collapse.Panel header="Embedding Expressions in JSX" key="2">
         <C010202></C010202>
+      </Collapse.Panel>
+
+      <Collapse.Panel header="JSX is an Expression Too" key="3">
+        <C010203></C010203>
       </Collapse.Panel>
     </Collapse>
   );
