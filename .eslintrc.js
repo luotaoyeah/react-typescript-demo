9a22b0ea8f7ec14f67611ee58c5b900e6a3b0dbe
luotao <luotaoyeah@gmail.com>
2019-07-12 17:29:25 +0800

    chore: update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ const { strictEslint } = require('@umijs/fabric');
 
 module.exports = {
   ...strictEslint,
+  rules: { '@typescript-eslint/no-inferrable-types': 0, 'import/prefer-default-export': 0, 'linebreak-style': 0 },
   globals: {
     page: true,
   },
