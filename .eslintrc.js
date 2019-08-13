8242383916dc76d7d1a64abcb9903fe6b695df6b
luotao <luotaoyeah@gmail.com>
2019-08-13 19:18:24 +0800

    ci: use mock in production

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,12 @@ const { strictEslint } = require('@umijs/fabric');
 
 module.exports = {
   ...strictEslint,
-  rules: { '@typescript-eslint/no-inferrable-types': 0, 'import/prefer-default-export': 0, 'linebreak-style': 0 },
+  rules: {
+    '@typescript-eslint/no-inferrable-types': 0,
+    'import/prefer-default-export': 0,
+    'linebreak-style': 0,
+    'import/no-unresolved': 0,
+  },
   globals: {
     page: true,
   },
--- src/services/user.ts
+++ src/services/user.ts
@@ -5,6 +5,34 @@ export async function query(): Promise<any> {
 }
 
 export async function queryCurrent(): Promise<any> {
+  if (process.env.NODE_ENV === 'production') {
+    return Promise.resolve({
+      name: 'luotao',
+      avatar: 'https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png',
+      userid: '00000001',
+      email: 'luotaoyeah@gmail.com',
+      signature: '海纳百川',
+      title: '专家',
+      group: '技术部',
+      tags: [],
+      notifyCount: 9,
+      unreadCount: 9,
+      country: '中国',
+      geographic: {
+        province: {
+          label: '四川省',
+          key: '330000',
+        },
+        city: {
+          label: '成都市',
+          key: '330100',
+        },
+      },
+      address: '西湖区',
+      phone: '028-88888888',
+    });
+  }
+
   return request('/api/currentUser');
 }
 
