94ec79d616eb32e948273e16936f32dc661b852f
luotao <luotaoyeah@gmail.com>
2019-08-16 14:39:37 +0800

    feat(doc.main-concepts): ReactDOM.render()

--- src/components/doc/01-main-concepts/03/01-03-02.tsx
+++ src/components/doc/01-main-concepts/03/01-03-02.tsx
@@ -1,15 +1,42 @@
+/*
+ * https://reactjs.org/docs/rendering-elements.html#rendering-an-element-into-the-dom
+ */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Button } from 'antd';
 
-/*
- * Rendering an Element into the DOM
- */
+// eslint-disable-next-line react/prefer-stateless-function
+export class C010302 extends React.Component {
+  public constructor(props: {}) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-/*
- * 通常情况下，一个 DOM 中只会创建一个 React 应用；
- * 但是 React 支持在一个 DOM 中创建多个相互隔离的 React 应用；
- * 使用 ReactDOM.render() 方法来创建应用，其中第二个参数表示该应用的'根节点'；
- */
-console.log('\n-------------------------------------------------- 01');
+  // eslint-disable-next-line class-methods-use-this
+  public handleClick() {
+    /*
+     * 使用 ReactDOM.render() 方法, 将一个 react element 渲染到某个 DOM 节点上去
+     */
+    ReactDOM.render(<span>foo</span>, document.querySelector('#p01'));
+  }
 
-ReactDOM.render(<h1>APP02</h1>, document.getElementById('root02'));
+  public render() {
+    return (
+      <>
+        <p>
+          <Button onClick={this.handleClick}>RENDER</Button>
+        </p>
+
+        <p
+          id="p01"
+          style={{
+            border: '1px solid red',
+            borderRadius: '3px',
+            padding: '10px',
+          }}
+        ></p>
+      </>
+    );
+  }
+}
--- src/components/doc/01-main-concepts/03/01-03.tsx
+++ src/components/doc/01-main-concepts/03/01-03.tsx
@@ -6,12 +6,17 @@ import React from 'react';
 import { Collapse, Icon } from 'antd';
 import './01-03.less';
 import { C010301 } from '@/components/doc/01-main-concepts/03/01-03-01';
+import { C010302 } from '@/components/doc/01-main-concepts/03/01-03-02';
 
 export default function C0103(): React.ReactNode {
   return (
     <Collapse accordion expandIcon={({ isActive }) => <Icon type="caret-right" rotate={isActive ? 90 : 0} />}>
       <Collapse.Panel header="Rendering Elements" key="1">
         <C010301></C010301>
+      </Collapse.Panel>
+
+      <Collapse.Panel header="Rendering an Element into the DOM" key="2">
+        <C010302></C010302>
       </Collapse.Panel>
     </Collapse>
   );
