ed43f6319c08f6635d96e3502c1fc9c30acab50b
luotao <luotaoyeah@gmail.com>
2019-08-20 17:12:25 +0800

    feat(doc.main-concepts): lessons learned

--- src/components/doc/01-main-concepts/10/01-10-02.tsx
+++ src/components/doc/01-main-concepts/10/01-10-02.tsx
@@ -1,14 +1,13 @@
 /*
- * Lifting State Up: Lessons Learned
+ * https://reactjs.org/docs/lifting-state-up.html#lessons-learned
  */
 
 import React from 'react';
 
 /*
- * 通常这样规划组件的状态数据：
- *     1. 首先将数据定义在 state 中；
- *     2. 如果数据需要跟其他组件共享，则将数据提升到父组件中，
- *        并通过 props 传入；
+ * 通常这样规划组件的状态数据:
+ *     1. 首先将数据定义在 state 中
+ *     2. 如果数据需要跟其他组件共享, 则将数据提升到上级组件中, 并通过 props 传入
  */
 
 function C011002() {
