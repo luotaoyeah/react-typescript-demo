0dbc973899cf7220da1e2204a6d3ecb3ef7879e0
luotao <luotaoyeah@gmail.com>
2019-08-21 19:38:34 +0800

    feat(doc.main-concepts): adding a ref to a DOM element

--- src/components/doc/02-advanced-guides/16/02-16.tsx
+++ src/components/doc/02-advanced-guides/16/02-16.tsx
@@ -5,12 +5,17 @@
 import React from 'react';
 import { Collapse, Icon } from 'antd';
 import { C021601 } from '@/components/doc/02-advanced-guides/16/02-16-01';
+import { C021602 } from '@/components/doc/02-advanced-guides/16/02-16-02';
 
 export default function C0108() {
   return (
     <Collapse accordion expandIcon={({ isActive }) => <Icon type="caret-right" rotate={isActive ? 90 : 0} />}>
       <Collapse.Panel header="Creating Refs" key="1">
         <C021601></C021601>
+      </Collapse.Panel>
+
+      <Collapse.Panel header="Adding a Ref to a DOM Element" key="2">
+        <C021602></C021602>
       </Collapse.Panel>
     </Collapse>
   );
