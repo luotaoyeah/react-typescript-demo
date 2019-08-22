81307de7383bd3967889a18f3a2ccfa2557b6b44
luotao <luotaoyeah@gmail.com>
2019-08-22 18:28:54 +0800

    feat(doc.main-concepts): motivation

--- src/components/doc/02-advanced-guides/06/02-06-02.tsx
+++ src/components/doc/02-advanced-guides/06/02-06-02.tsx
@@ -1,33 +1,30 @@
 /*
- * Fragments: Motivation
+ * https://reactjs.org/docs/fragments.html#motivation
  */
 
 import React from 'react';
 
 function C020602() {
   /*
-   * 因为 render() 方法返回的必须是单个节点，
-   * 所以如果要返回多个节点，必须用一个节点将所有的节点包裹起来，
-   * 比如最外层使用 div 包裹，
-   * 这种方式的问题在于，会产生多余的 div，导致某些 HTML 非法，
-   * 如：<tbody><div><tr></tr><tr></tr></div></tbody>
+   * 因为 render() 方法返回的必须是单个节点, 所以如果要返回多个节点, 必须用一个父节点将所有的节点包裹起来, 比如最外层使用 div 包裹,
+   * 这种方式的问题在于, 会产生一层多余的 div 标签, 导致某些 HTML 非法, 比如：<tbody><div><tr></tr><tr></tr></div></tbody>,
+   * 使用 React.Fragment 组件来作为父节点, 就不会产生额外的标签
    */
 
   const tds = (
     <>
-      <td>D</td>
-      <td>E</td>
+      <td>A</td>
+      <td>B</td>
+      <td>C</td>
     </>
   );
 
   return (
-    <div>
-      <table>
-        <tbody>
-          <tr>{tds}</tr>
-        </tbody>
-      </table>
-    </div>
+    <table>
+      <tbody>
+        <tr>{tds}</tr>
+      </tbody>
+    </table>
   );
 }
 
--- src/components/doc/02-advanced-guides/06/02-06.tsx
+++ src/components/doc/02-advanced-guides/06/02-06.tsx
@@ -5,12 +5,17 @@
 import React from 'react';
 import { Collapse, Icon } from 'antd';
 import { C020601 } from '@/components/doc/02-advanced-guides/06/02-06-01';
+import { C020602 } from '@/components/doc/02-advanced-guides/06/02-06-02';
 
 export default function C0108() {
   return (
     <Collapse accordion expandIcon={({ isActive }) => <Icon type="caret-right" rotate={isActive ? 90 : 0} />}>
       <Collapse.Panel header="Fragments" key="1">
         <C020601></C020601>
+      </Collapse.Panel>
+
+      <Collapse.Panel header="Motivation" key="2">
+        <C020602></C020602>
       </Collapse.Panel>
     </Collapse>
   );
