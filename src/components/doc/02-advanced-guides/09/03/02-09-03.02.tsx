71c845fde1cce170ad619066e28117562419cdbf
luotao <luotaoyeah@gmail.com>
2019-08-24 15:01:02 +0800

    feat(doc.advanced-guides): jsx: string literals

---
+++ src/components/doc/02-advanced-guides/09/03/02-09-03.02.tsx
@@ -0,0 +1,33 @@
+/*
+ * https://reactjs.org/docs/jsx-in-depth.html#string-literals
+ */
+import React from 'react';
+import { Button } from 'antd';
+
+interface IProps {
+  foo?: string;
+  bar?: string;
+}
+
+function C02090301A({ foo, bar }: IProps) {
+  return (
+    <>
+      <Button type="primary">{foo}</Button> <Button type="danger">{bar}</Button>
+    </>
+  );
+}
+
+/*
+ * props 的值可以是一个字符串, 这时候有两种形式的写法:
+ *   1. prop="value"
+ *   2. prop={"value"}
+ *
+ * 区别在于, 第二种形式不会对特殊字符进行转义, 而是原样显示
+ */
+
+function C02090302() {
+  // eslint-disable-next-line react/jsx-curly-brace-presence
+  return <C02090301A foo="<div&gt;" bar={'<div&gt;'}></C02090301A>;
+}
+
+export { C02090302 };
--- src/components/doc/02-advanced-guides/09/03/02-09-03.tsx
+++ src/components/doc/02-advanced-guides/09/03/02-09-03.tsx
@@ -5,12 +5,17 @@
 import React from 'react';
 import { Collapse, Icon } from 'antd';
 import { C02090301 } from '@/components/doc/02-advanced-guides/09/03/02-09-03.01';
+import { C02090302 } from '@/components/doc/02-advanced-guides/09/03/02-09-03.02';
 
 function C020903() {
   return (
     <Collapse accordion expandIcon={({ isActive }) => <Icon type="caret-right" rotate={isActive ? 90 : 0} />}>
       <Collapse.Panel header="JavaScript Expressions as Props" key="1">
         <C02090301></C02090301>
+      </Collapse.Panel>
+
+      <Collapse.Panel header="String Literals" key="2">
+        <C02090302></C02090302>
       </Collapse.Panel>
     </Collapse>
   );
