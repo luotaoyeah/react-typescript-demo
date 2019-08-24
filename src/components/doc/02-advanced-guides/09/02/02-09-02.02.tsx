e8249e5acfda7a9313d346d9de4d5ee58886c5f0
luotao <luotaoyeah@gmail.com>
2019-08-24 12:07:04 +0800

    feat(doc.advanced-guides): jsx: using dot notation for JSX type

---
+++ src/components/doc/02-advanced-guides/09/02/02-09-02.02.tsx
@@ -0,0 +1,28 @@
+/*
+ * https://reactjs.org/docs/jsx-in-depth.html#using-dot-notation-for-jsx-type
+ */
+
+import React from 'react';
+import { Button } from 'antd';
+
+/*
+ * 组件的标签名中可以包含点运算符, 比如内置的组件 <React.Fragment>,
+ * 最后一个点前面的表示各级命名空间, 最后一个点后面的才表示真正的组件
+ */
+
+const C02090202A = {
+  A: ({ children }: JSX.ElementChildrenAttribute) => <Button type="primary">{children}</Button>,
+  B: {
+    C: ({ children }: JSX.ElementChildrenAttribute) => <Button type="danger">{children}</Button>,
+  },
+};
+
+function C02090202() {
+  return (
+    <React.Fragment>
+      <C02090202A.A>A</C02090202A.A> <C02090202A.B.C>B</C02090202A.B.C>
+    </React.Fragment>
+  );
+}
+
+export { C02090202 };
--- src/components/doc/02-advanced-guides/09/02/02-09-02.tsx
+++ src/components/doc/02-advanced-guides/09/02/02-09-02.tsx
@@ -5,12 +5,17 @@
 import React from 'react';
 import { Collapse, Icon } from 'antd';
 import { C02090201 } from '@/components/doc/02-advanced-guides/09/02/02-09-02.01';
+import { C02090202 } from '@/components/doc/02-advanced-guides/09/02/02-09-02.02';
 
 function C020902() {
   return (
     <Collapse accordion expandIcon={({ isActive }) => <Icon type="caret-right" rotate={isActive ? 90 : 0} />}>
       <Collapse.Panel header="React Must Be in Scope" key="1">
         <C02090201></C02090201>
+      </Collapse.Panel>
+
+      <Collapse.Panel header="Using Dot Notation for JSX Type" key="2">
+        <C02090202></C02090202>
       </Collapse.Panel>
     </Collapse>
   );
