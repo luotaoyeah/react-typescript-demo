d511131562077da4605bfb7a1d755f02b1a8f6ad
luotao <luotaoyeah@gmail.com>
2019-08-25 13:56:30 +0800

    feat(doc.advanced-guides): context: before you use context

--- src/components/doc/02-advanced-guides/03/02-03-02.tsx
+++ src/components/doc/02-advanced-guides/03/02-03-02.tsx
@@ -1,80 +1,82 @@
 /*
- * Context: Before You Use Context
+ * https://reactjs.org/docs/context.html#before-you-use-context
  */
 
 import React from 'react';
 import { Input } from 'antd';
+import { Divider } from 'antd/es';
 
 /*
- * context 在多个不同层级的组件需要共享数据时使用，
- * 缺点是，会使组件变得难以重用；
+ * 适合使用 context 的场景是: 多个不同层次的组件需要共用数据,
+ * 如果仅仅是为了避免层层传递数据, 可能 component composition 是更好的方式
  */
 
 /*
- * 使用 props 层层传递的方式
+ * 层层传递 props 属性
  */
-function A(props: { name: string; age: number }) {
-  const { age, name } = props;
-  return <B name={name} age={age} />;
+
+interface IProps01 {
+  name: string;
+  age: number;
 }
 
-function B(props: { name: string; age: number }) {
-  const { age, name } = props;
-  return <C name={name} age={age} />;
+function C020302A01({ age, name }: IProps01) {
+  return <C020302B01 name={name} age={age} />;
 }
 
-function C(props: { name: string; age: number }) {
-  const { age, name } = props;
+function C020302B01({ age, name }: IProps01) {
+  return <C020302C01 name={name} age={age} />;
+}
+
+function C020302C01({ age, name }: IProps01) {
   return (
-    <div>
-      <p>
-        <Input addonBefore="name：" defaultValue={name} />
-      </p>
-      <p>
-        <Input addonBefore="age：" defaultValue={age.toString()} />
-      </p>
-    </div>
+    <>
+      <Input addonBefore="name：" defaultValue={name} />
+      <Input addonBefore="age：" defaultValue={age.toString()} />
+    </>
   );
 }
 
 /*
  * 传递整个组件
  */
-function A2(props: { form: React.ReactNode }) {
-  const { form } = props;
-  return <B2 form={form} />;
+
+interface IProps02 {
+  form: React.ReactNode;
 }
 
-function B2(props: { form: React.ReactNode }) {
-  const { form } = props;
-  return <C2 form={form} />;
+function C020302A02({ form }: IProps02) {
+  return <C020302B02 form={form} />;
 }
 
-class C2 extends React.Component<{ form: React.ReactNode }> {
+function C020302B02({ form }: IProps02) {
+  return <C020302C02 form={form} />;
+}
+
+class C020302C02 extends React.Component<IProps02> {
   public render() {
     const { form } = this.props;
+
     return form;
   }
 }
 
 function C020302() {
   return (
-    <div>
-      <A name="tom" age={18} />
-      <hr />
-      <A2
+    <>
+      <C020302A01 name="tom" age={18} />
+
+      <Divider />
+
+      <C020302A02
         form={
-          <div>
-            <p>
-              <Input addonBefore="name：" defaultValue="tom" />
-            </p>
-            <p>
-              <Input addonBefore="age：" defaultValue="18" />
-            </p>
-          </div>
+          <>
+            <Input addonBefore="name：" defaultValue="tom" />
+            <Input addonBefore="age：" defaultValue="18" />
+          </>
         }
       />
-    </div>
+    </>
   );
 }
 
--- src/components/doc/02-advanced-guides/03/02-03.tsx
+++ src/components/doc/02-advanced-guides/03/02-03.tsx
@@ -5,12 +5,17 @@
 import React from 'react';
 import { Collapse, Icon } from 'antd';
 import { C020301 } from '@/components/doc/02-advanced-guides/03/01/02-03-01';
+import { C020302 } from '@/components/doc/02-advanced-guides/03/02-03-02';
 
 export default function C0203() {
   return (
     <Collapse accordion expandIcon={({ isActive }) => <Icon type="caret-right" rotate={isActive ? 90 : 0} />}>
       <Collapse.Panel header="When to Use Context" key="1">
         <C020301></C020301>
+      </Collapse.Panel>
+
+      <Collapse.Panel header="Before You Use Context" key="2">
+        <C020302></C020302>
       </Collapse.Panel>
     </Collapse>
   );
