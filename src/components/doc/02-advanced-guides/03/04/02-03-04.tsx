c5aeaeb2e4747cd8d8a94e0ee7cbbc8a74d6e927
luotao <luotaoyeah@gmail.com>
2019-08-25 15:55:16 +0800

    feat(doc.advanced-guides): context: updating context from a nested component

--- src/components/doc/02-advanced-guides/03/04/02-03-04.tsx
+++ src/components/doc/02-advanced-guides/03/04/02-03-04.tsx
@@ -5,12 +5,17 @@
 import React from 'react';
 import { Collapse, Icon } from 'antd';
 import { C02030401 } from '@/components/doc/02-advanced-guides/03/04/02-03-04.01';
+import { C02030402 } from '@/components/doc/02-advanced-guides/03/04/02-03-04.02';
 
 function C020304() {
   return (
     <Collapse accordion expandIcon={({ isActive }) => <Icon type="caret-right" rotate={isActive ? 90 : 0} />}>
       <Collapse.Panel header="Dynamic Context" key="1">
         <C02030401></C02030401>
+      </Collapse.Panel>
+
+      <Collapse.Panel header="Updating Context from a Nested Component" key="2">
+        <C02030402></C02030402>
       </Collapse.Panel>
     </Collapse>
   );
