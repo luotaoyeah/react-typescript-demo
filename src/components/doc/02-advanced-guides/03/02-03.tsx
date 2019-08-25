d819e20ec15381e4781b8e5098ae84e340b10595
luotao <luotaoyeah@gmail.com>
2019-08-25 16:19:46 +0800

    feat(doc.advanced-guides): context: caveats

--- src/components/doc/02-advanced-guides/03/02-03.tsx
+++ src/components/doc/02-advanced-guides/03/02-03.tsx
@@ -8,6 +8,7 @@ import { C020301 } from '@/components/doc/02-advanced-guides/03/01/02-03-01';
 import { C020302 } from '@/components/doc/02-advanced-guides/03/02-03-02';
 import { C020303 } from '@/components/doc/02-advanced-guides/03/03/02-03-03';
 import { C020304 } from '@/components/doc/02-advanced-guides/03/04/02-03-04';
+import { C020305 } from '@/components/doc/02-advanced-guides/03/02-03-05';
 
 export default function C0203() {
   return (
@@ -26,6 +27,10 @@ export default function C0203() {
 
       <Collapse.Panel header="Examples" key="4">
         <C020304></C020304>
+      </Collapse.Panel>
+
+      <Collapse.Panel header="Caveats" key="5">
+        <C020305></C020305>
       </Collapse.Panel>
     </Collapse>
   );
