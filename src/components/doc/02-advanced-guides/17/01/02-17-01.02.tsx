226b2a0e2553ebbd200af7d7e808601cd17f6e6d
luotao <luotaoyeah@gmail.com>
2019-08-25 10:31:51 +0800

    feat(doc.advanced-guides): render props: HOC

---
+++ src/components/doc/02-advanced-guides/17/01/02-17-01.02.tsx
@@ -0,0 +1,67 @@
+/*
+ * https://reactjs.org/docs/render-props.html#use-render-props-for-cross-cutting-concerns
+ */
+
+import React from 'react';
+import { Button } from 'antd';
+import { ButtonProps } from 'antd/es/button';
+
+/*
+ * 有了 render props 之后, 大部分使用 HOC 的地方都可以使用 render props 来实现,
+ * 如果确实需要使用 HOC, 可以在 HOC 的内部使用 render props 来实现
+ */
+
+interface IProps {
+  render?: (state: IState) => React.ReactNode;
+}
+
+interface IState {
+  text: string;
+}
+
+class C02170102A extends React.Component<IProps, IState> {
+  public constructor(props: Readonly<IProps>) {
+    super(props);
+
+    this.state = { text: 'C02170102A' };
+  }
+
+  public render() {
+    const { render: renderProp } = this.props;
+    if (renderProp) {
+      return renderProp(this.state);
+    }
+
+    const { text } = this.state;
+    return <Button>{text}</Button>;
+  }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function withSomething(C01: any) {
+  return function C02(props: ButtonProps) {
+    return <C02170102A render={(state: IState) => <C01 {...props}>{state.text}</C01>}></C02170102A>;
+  };
+}
+
+const C02170102B = withSomething(Button);
+
+function C02170102() {
+  return (
+    <>
+      <C02170102A />
+
+      <C02170102A
+        render={(state: IState) => (
+          <Button type="primary" style={{ marginLeft: '10px' }}>
+            {state.text}
+          </Button>
+        )}
+      />
+
+      <C02170102B type="danger" style={{ marginLeft: '10px' }} />
+    </>
+  );
+}
+
+export { C02170102 };
--- src/components/doc/02-advanced-guides/17/01/02-17-01.tsx
+++ src/components/doc/02-advanced-guides/17/01/02-17-01.tsx
@@ -3,68 +3,20 @@
  */
 
 import React from 'react';
-import { Button } from 'antd';
-import { IMouseState, Mouse } from './mouse';
-import { withMouse } from './with-mouse';
+import { Divider } from 'antd';
 import { C02170101 } from '@/components/doc/02-advanced-guides/17/01/02-17-01.01';
+import { C02170102 } from '@/components/doc/02-advanced-guides/17/01/02-17-01.02';
 
-/*
- * render props 指的是一种特殊的 props，
- * 这个属性通常取名为 render，它的作用是：
- *     覆盖组件自身的 render() 方法；
- *
- * render props 接收组件的 state 对象作为参数，
- * 返回一个 React 元素，组件可以使用该 render props 个性化组件的渲染，
- * 即将组件的渲染逻辑参数化，使用外界传入的方法来进行渲染；
- */
-
-function CatButton(props: { mouse: IMouseState }) {
-  const { mouse } = props;
+function C021701() {
   return (
-    <Button
-      style={{
-        position: 'absolute',
-        left: `${mouse.x - 42 + window.scrollX}px`,
-        top: `${mouse.y - 32 + window.scrollY}px`,
-        transition: 'none',
-      }}
-    >
-      B
-    </Button>
+    <>
+      <C02170101></C02170101>
+
+      <Divider />
+
+      <C02170102></C02170102>
+    </>
   );
-}
-
-const CatButtonWithMouse = withMouse(CatButton);
-
-/**  */
-class C021701 extends React.Component {
-  // eslint-disable-next-line class-methods-use-this
-  public renderMouse(state: any) {
-    return (
-      <Button
-        style={{
-          position: 'absolute',
-          left: `${state.x - 42 + window.scrollX}px`,
-          top: `${state.y - 32 + window.scrollY}px`,
-          transition: 'none',
-        }}
-      >
-        A
-      </Button>
-    );
-  }
-
-  public render() {
-    return (
-      <>
-        <C02170101></C02170101>
-
-        <Mouse />
-        <Mouse render={this.renderMouse} />
-        <CatButtonWithMouse />
-      </>
-    );
-  }
 }
 
 export { C021701 };
