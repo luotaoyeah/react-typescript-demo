9feebb3388a4cc51b5c6a0be2a29905a6368b0e8
luotao <luotaoyeah@gmail.com>
2019-08-26 19:39:32 +0800

    feat(doc.api-reference): React.Component: render(): React.ReactPortal

--- src/components/doc/03-api-reference/02/02/03-02-02.tsx
+++ src/components/doc/03-api-reference/02/02/03-02-02.tsx
@@ -7,6 +7,7 @@ import { Divider } from 'antd';
 import { C03020201 } from '@/components/doc/03-api-reference/02/02/03-02-02.01';
 import { C03020202 } from '@/components/doc/03-api-reference/02/02/03-02-02.02';
 import { C03020203 } from '@/components/doc/03-api-reference/02/02/03-02-02.03';
+import { C03020204 } from '@/components/doc/03-api-reference/02/02/03-02-02.04';
 
 function C030202() {
   return (
@@ -20,6 +21,10 @@ function C030202() {
       <Divider />
 
       <C03020203></C03020203>
+
+      <Divider />
+
+      <C03020204></C03020204>
     </>
   );
 }
