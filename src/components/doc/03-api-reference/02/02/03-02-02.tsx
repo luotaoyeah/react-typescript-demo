66585111c27a11419c479d38a002bf7d6906c633
luotao <luotaoyeah@gmail.com>
2019-08-26 18:44:05 +0800

    feat(doc.api-reference): React.Component: render()

---
+++ src/components/doc/03-api-reference/02/02/03-02-02.tsx
@@ -0,0 +1,16 @@
+/*
+ * https://reactjs.org/docs/react-component.html#render
+ */
+
+import React from 'react';
+import { C03020201 } from '@/components/doc/03-api-reference/02/02/03-02-02.01';
+
+function C030202() {
+  return (
+    <>
+      <C03020201></C03020201>
+    </>
+  );
+}
+
+export { C030202 };
--- src/components/doc/03-api-reference/02/03-02.tsx
+++ src/components/doc/03-api-reference/02/03-02.tsx
@@ -5,12 +5,17 @@
 import React from 'react';
 import { Collapse, Icon } from 'antd';
 import { C030201 } from '@/components/doc/03-api-reference/02/03-02-01';
+import { C030202 } from '@/components/doc/03-api-reference/02/02/03-02-02';
 
 export default function C0302() {
   return (
     <Collapse accordion expandIcon={({ isActive }) => <Icon type="caret-right" rotate={isActive ? 90 : 0} />}>
       <Collapse.Panel header="Overview" key="1">
         <C030201></C030201>
+      </Collapse.Panel>
+
+      <Collapse.Panel header="render()" key="2">
+        <C030202></C030202>
       </Collapse.Panel>
     </Collapse>
   );
