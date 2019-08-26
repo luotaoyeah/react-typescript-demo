c5ab4585ccca91508b1883c73181a7021018363f
luotao <luotaoyeah@gmail.com>
2019-08-26 19:13:24 +0800

    feat(doc.api-reference): React.Component: render(): React.Fragment

--- src/components/doc/03-api-reference/02/02/03-02-02.tsx
+++ src/components/doc/03-api-reference/02/02/03-02-02.tsx
@@ -6,6 +6,7 @@ import React from 'react';
 import { Divider } from 'antd';
 import { C03020201 } from '@/components/doc/03-api-reference/02/02/03-02-02.01';
 import { C03020202 } from '@/components/doc/03-api-reference/02/02/03-02-02.02';
+import { C03020203 } from '@/components/doc/03-api-reference/02/02/03-02-02.03';
 
 function C030202() {
   return (
@@ -15,6 +16,10 @@ function C030202() {
       <Divider />
 
       <C03020202></C03020202>
+
+      <Divider />
+
+      <C03020203></C03020203>
     </>
   );
 }
