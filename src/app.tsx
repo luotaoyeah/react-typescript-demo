62ab12c5ecbd7b2c7f34bfc3c9e5bb33659495d0
luotao <luotaoyeah@gmail.com>
2019-09-29 13:28:09 +0800

    refactor: scaffold

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,21 @@
+import "./app.less";
 import * as React from "react";
 import { Button, Dropdown, Menu } from "antd";
+import { Route } from "react-router";
+import { Link } from "react-router-dom";
+import { C010201 } from "./views/doc/01-main-concepts/01-02/01-02-01";
 
 class App extends React.Component {
   public render() {
     const menu = (
       <Menu>
         <Menu.SubMenu title="DOC">
-          <Menu.Item>3rd menu item</Menu.Item>
-          <Menu.Item>4th menu item</Menu.Item>
+          <Menu.SubMenu title="MAIN CONCEPTS">
+            <Menu.Item>Hello World</Menu.Item>
+            <Menu.Item>
+              <Link to={"/docs/introducing-jsx"}>Introducing JSX</Link>
+            </Menu.Item>
+          </Menu.SubMenu>
         </Menu.SubMenu>
 
         <Menu.SubMenu title="PACKAGES">
@@ -19,12 +27,16 @@ class App extends React.Component {
     );
 
     return (
-      <div className="App">
+      <div className="app">
         <Dropdown overlay={menu}>
           <Button onClick={e => e.preventDefault()} style={{ margin: "10px" }}>
             目录
           </Button>
         </Dropdown>
+
+        <div style={{ height: "calc(100% - 52px)", padding: "10px" }}>
+          <Route path={"/docs/introducing-jsx"} component={C010201}></Route>
+        </div>
       </div>
     );
   }
