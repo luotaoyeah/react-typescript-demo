ffd3d1701315e7cc04853cf04a1fa573cdbf8836
luotao <luotaoyeah@gmail.com>
2019-11-30 09:43:00 +0800

    refactor(doc): adding-local-state-to-a-class

---
+++ src/views/doc/main-concepts/state-and-lifecycle/adding-local-state-to-a-class.component.tsx
@@ -0,0 +1,35 @@
+// https://reactjs.org/docs/state-and-lifecycle.html#adding-local-state-to-a-class
+
+import * as React from "react";
+
+interface IProps {}
+
+interface IState {
+  date: Date;
+}
+
+class Clock extends React.Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+
+    this.state = {
+      date: new Date(),
+    };
+  }
+
+  public render() {
+    return <div>{this.state.date.toISOString()}</div>;
+  }
+}
+
+class AddingLocalStateToAClassComponent extends React.Component {
+  public render() {
+    return (
+      <div>
+        <Clock />
+      </div>
+    );
+  }
+}
+
+export { AddingLocalStateToAClassComponent };
--- src/views/doc/main-concepts/state-and-lifecycle/state-and-lifecycle.component.tsx
+++ src/views/doc/main-concepts/state-and-lifecycle/state-and-lifecycle.component.tsx
@@ -4,6 +4,7 @@ import * as React from "react";
 import { Collapse } from "antd";
 import { CaretRightOutlined } from "@ant-design/icons";
 import { ConvertingAFunctionToAClassComponent } from "./converting-a-function-to-a-class.component";
+import { AddingLocalStateToAClassComponent } from "./adding-local-state-to-a-class.component";
 
 class StateAndLifecycleComponent extends React.Component {
   public render() {
@@ -11,6 +12,10 @@ class StateAndLifecycleComponent extends React.Component {
       <Collapse expandIcon={({ isActive }) => <CaretRightOutlined rotate={isActive ? 90 : 0} />} accordion={true}>
         <Collapse.Panel header="converting-a-function-to-a-class" key="1">
           <ConvertingAFunctionToAClassComponent />
+        </Collapse.Panel>
+
+        <Collapse.Panel header="adding-local-state-to-a-class" key="2">
+          <AddingLocalStateToAClassComponent />
         </Collapse.Panel>
       </Collapse>
     );
