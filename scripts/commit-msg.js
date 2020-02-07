59f5b2ee47357e63d2947810dcc9fde98209b0d8
luotao <luotaoyeah@gmail.com>
2020-02-07 20:47:29 +0800

    refactor: consistent with standard specification

--- scripts/commit-msg.js
+++ scripts/commit-msg.js
@@ -1,17 +1,7 @@
-const chalk = require("chalk");
-const messagePath = process.env.HUSKY_GIT_PARAMS;
-const message = require("fs")
-  .readFileSync(messagePath, "utf-8")
-  .trim();
-
-const commitRegExp = /^(revert: )?(feat|fix|polish|docs|style|refactor|perf|test|workflow|ci|chore|types|build)(\(.+\))?: .{1,50}/;
+const message = require('fs').readFileSync(process.env.HUSKY_GIT_PARAMS, 'utf-8').trim();
+const commitRegExp = /^(revert: )?(Merge|feat|fix|polish|docs|style|refactor|perf|test|workflow|ci|chore|types|build)(\(.+\))?: .{1,50}/;
 
 if (!commitRegExp.test(message)) {
-  console.log("\n--------------------------------------------------");
-
-  console.error(`  ${chalk.bgRed.white("ERROR")} ${chalk.red(`INVALID COMMIT MESSAGE FORMAT`)}`);
-
-  console.log("--------------------------------------------------\n");
-
+  console.error('\nERROR: INVALID COMMIT MESSAGE FORMAT.\n');
   process.exit(1);
 }
--- src/app.tsx
+++ src/app.tsx
@@ -1,30 +1,30 @@
-import "./app.less";
-import * as React from "react";
-import { Button, Menu, Popover } from "antd";
-import { Route } from "react-router";
-import { Link } from "react-router-dom";
-import { IntroducingJsxComponent } from "./views/doc/main-concepts/introducing-jsx/introducing-jsx.component";
-import { RenderingElementsComponent } from "./views/doc/main-concepts/rendering-elements/rendering-elements.component";
-import { ComponentsAndPropsComponent } from "./views/doc/main-concepts/components-and-props/components-and-props.component";
-import { StateAndLifecycleComponent } from "./views/doc/main-concepts/state-and-lifecycle/state-and-lifecycle.component";
+import './app.less';
+import * as React from 'react';
+import { Button, Menu, Popover } from 'antd';
+import { Route } from 'react-router';
+import { Link } from 'react-router-dom';
+import { IntroducingJsxComponent } from './views/doc/main-concepts/introducing-jsx/introducing-jsx.component';
+import { RenderingElementsComponent } from './views/doc/main-concepts/rendering-elements/rendering-elements.component';
+import { ComponentsAndPropsComponent } from './views/doc/main-concepts/components-and-props/components-and-props.component';
+import { StateAndLifecycleComponent } from './views/doc/main-concepts/state-and-lifecycle/state-and-lifecycle.component';
 
 class App extends React.Component {
   public render() {
     const menu = (
-      <Menu openTransitionName="" openAnimation="" mode={"inline"}>
+      <Menu openTransitionName="" openAnimation="" mode={'inline'}>
         <Menu.SubMenu title="DOC">
-          <Menu.SubMenu title={"MAIN CONCEPTS"}>
+          <Menu.SubMenu title={'MAIN CONCEPTS'}>
             <Menu.Item>
-              <Link to={"/docs/introducing-jsx"}>introducing-jsx</Link>
+              <Link to={'/docs/introducing-jsx'}>introducing-jsx</Link>
             </Menu.Item>
             <Menu.Item>
-              <Link to={"/docs/rendering-elements"}>rendering-elements</Link>
+              <Link to={'/docs/rendering-elements'}>rendering-elements</Link>
             </Menu.Item>
             <Menu.Item>
-              <Link to={"/docs/components-and-props"}>components-and-props</Link>
+              <Link to={'/docs/components-and-props'}>components-and-props</Link>
             </Menu.Item>
             <Menu.Item>
-              <Link to={"/docs/state-and-lifecycle"}>state-and-lifecycle</Link>
+              <Link to={'/docs/state-and-lifecycle'}>state-and-lifecycle</Link>
             </Menu.Item>
           </Menu.SubMenu>
         </Menu.SubMenu>
@@ -46,16 +46,16 @@ class App extends React.Component {
           trigger="click"
           overlayClassName="popover-content-menu"
         >
-          <Button type="primary" style={{ margin: "10px", width: "calc(100% - 20px)" }}>
+          <Button type="primary" style={{ margin: '10px', width: 'calc(100% - 20px)' }}>
             目录
           </Button>
         </Popover>
 
-        <div style={{ height: "calc(100% - 52px)", padding: "10px" }}>
-          <Route path={"/docs/introducing-jsx"} component={IntroducingJsxComponent}></Route>
-          <Route path={"/docs/rendering-elements"} component={RenderingElementsComponent}></Route>
-          <Route path={"/docs/components-and-props"} component={ComponentsAndPropsComponent}></Route>
-          <Route path={"/docs/state-and-lifecycle"} component={StateAndLifecycleComponent}></Route>
+        <div style={{ height: 'calc(100% - 52px)', padding: '10px' }}>
+          <Route path={'/docs/introducing-jsx'} component={IntroducingJsxComponent}></Route>
+          <Route path={'/docs/rendering-elements'} component={RenderingElementsComponent}></Route>
+          <Route path={'/docs/components-and-props'} component={ComponentsAndPropsComponent}></Route>
+          <Route path={'/docs/state-and-lifecycle'} component={StateAndLifecycleComponent}></Route>
         </div>
       </div>
     );
