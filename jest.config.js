81e53ba8de2afa1ad8016d33a6dae077437908aa
luotao <luotaoyeah@gmail.com>
2020-02-09 20:53:10 +0800

    chore: upgrade to react@17.0.1

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   collectCoverage: true,
-  collectCoverageFrom: ["src/**/*.{js,jsx,ts,tsx}", "!**/node_modules/**"],
-  coverageReporters: ["lcovonly", "text-summary"],
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!**/node_modules/**'],
+  coverageReporters: ['lcovonly', 'text-summary'],
+  transformIgnorePatterns: ['/node_modules/(?!antd|@ant-design|rc-.+?|@babel/runtime).+(js|jsx)$'],
 };
